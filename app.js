f74595ccbaf116c714200ae287ee70e0d93d8a48
Dani <daniaxasabraxas@gmail.com>
2020-08-25 16:26:29 -0400

    first commit

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
-const productRoutes = require('./api/routes/products')
-const orderRoutes = require('./api/routes/orders')
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+// Morgan allow us watch the request in the console.
+app.use(morgan('dev'));
+
+// Routes which should handle request
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
