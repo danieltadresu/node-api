b4ae77ac15e7b7cff33a3caecdd1f730cf67dcbe
Dani <daniaxasabraxas@gmail.com>
2020-08-25 18:12:51 -0400

    getting the errors at the app.js file

--- app.js
+++ app.js
@@ -5,11 +5,26 @@ const morgan = require('morgan');
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
-// Morgan allow us watch the request in the console.
+// Morgan allow us PRINT the request in the console.
 app.use(morgan('dev'));
 
 // Routes which should handle request
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
