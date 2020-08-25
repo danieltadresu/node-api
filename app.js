266e8de7da280c75ef14db2bb474df725cf1d204
Dani <daniaxasabraxas@gmail.com>
2020-08-25 18:40:46 -0400

    body parser and CORS errors in app.js file

--- app.js
+++ app.js
@@ -2,11 +2,34 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
+const bodyParser = require('body-parser');
+
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 // Morgan allow us PRINT the request in the console.
 app.use(morgan('dev'));
+
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(bodyParser.json());
+
+/* If we have CORS ERRORS, use this */
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({});
+  }
+  next();
+});
 
 // Routes which should handle request
 app.use('/products', productRoutes);
