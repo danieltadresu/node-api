81d94aa8ce2d1a75514f7c070a6a8c16656c9221
Dani <daniaxasabraxas@gmail.com>
2020-08-26 20:01:18 -0400

    adding controllers

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,6 +6,8 @@ const Product = require('../models/product');
 
 const multer = require('multer');
 
+const ProductsController = require('../controllers/products')
+
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
     cb(null, './uploads/');
@@ -32,79 +34,16 @@ const upload = multer({
   fileFilter : fileFilter
 });
 
-router.get('/', (req, res, next) => {
-  Product.find()
-  .exec()
-  .then(docs => {
-    console.log(docs);
-    res.status(200).json(docs)
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(500).json({
-      error: err
-    });
-  });
-});
+router.get('/', ProductsController.products_get_all);
 
-router.post('/', upload.single('productImage'), (req, res, next) => {
-  console.log(req.file);
-   const product = new Product({
-     _id: new mongoose.Types.ObjectId(),
-     name: req.body.name,
-     price: req.body.price,
-     productImage: req.file.path
-   });
+router.post('/', upload.single('productImage'), ProductsController.products_create_product);
 
-   product.save().then(result => {
-     console.log(result);
-     res.status(201).json({
-         message: 'Handling POST requests to /products',
-         createdProduct: result
-     });
-   })
-   .catch(err => {
-     console.log(err);
-     res.status(500).json({
-       error: err
-     });
-   });
-});
-
-router.get('/:productId', (req, res, next) => {
-  const id = req.params.productId;
-  Product.findById(id)
-  .exec()
-  .then(doc => {
-    console.log(doc);
-    if (doc) {
-      res.status(200).json(doc);
-    }
-    else {
-      res.status(404).json({message: 'No valid entry found for provided ID'})
-    }
-  }).catch(err => {
-    console.log(err)
-    res.status(500).json({error: err});
-  });
-});
+router.get('/:productId', ProductsController.products_get_product);
 
 router.patch('/:productId', (req, res, next) => {
   /* Request to modify, not yet */
 });
 
-router.delete('/:productId', (req, res, next) => {
-  const id = req.params.productId;
-  Product.remove({
-    _id: id
-  }).exec().then(result => {
-    res.status(200).json(result)
-  }).catch(err => {
-    console.log(err);
-    res.status(500).json({
-      error: err
-    })
-  });
-});
+router.delete('/:productId', ProductsController.products_delete);
 
 module.exports = router;
